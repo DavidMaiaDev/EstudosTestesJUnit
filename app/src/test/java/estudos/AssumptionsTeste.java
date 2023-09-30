4bdc47759a03f5f0962b76633e5b064f6cbef189
David Gonçalves Maia <david.goncalves@dcx.ufpb.br>
2023-09-30 11:47:25 -0300

    Mais estudos

---
+++ app/src/test/java/estudos/AssumptionsTeste.java
@@ -0,0 +1,14 @@
+package estudos;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+
+public class AssumptionsTeste {
+
+    @Test
+    void validarAlgoSomenteNoUsuarioDavid() {
+        Assumptions.assumeFalse("root".equals(System.getenv("USER")));
+        Assertions.assertEquals(10, 5 + 5);
+    }
+}
